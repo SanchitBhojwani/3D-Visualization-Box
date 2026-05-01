d70cacb5fc38cec203605bd251f4dedede18225e
Sanchit <sbdec2005@gmail.com>
2026-05-01 21:06:50 +0530

    mpu6050 driver

--- i2c_mpu6050/Core/Src/mpu6050.c
+++ i2c_mpu6050/Core/Src/mpu6050.c
@@ -10,13 +10,13 @@ int mpu_init(void){
 	HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDR, PWR_MGMT_1, 1, &wake , 1, 100);
 
 	uint8_t data = 0x00;
-	HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDR , ACCEL_CONFIG , 1 , &data , 1 , 100 );
+	HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDR , ACCEL_CONFIG , 1 , &data , 1 , 100 ); //setting accel measurement range to +-2g
 
 	uint8_t data = 0x00;
-	HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDR , GYRO_CONFIG , 1 , &data , 1 , 100 );
+	HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDR , GYRO_CONFIG , 1 , &data , 1 , 100 ); // setting gyro measurement range to ±250°/s
 
 	uint8_t data = 0x03;
-	HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDR , CONFIG , 1 , &data , 1 , 100 );
+	HAL_I2C_Mem_Write(&hi2c2, MPU6050_ADDR , CONFIG , 1 , &data , 1 , 100 ); // reduces high-frequency noise inside sensor DLPF (low-pass filter)
 
 	return 0;
 }
